1190f6a0c024b74e349712314de404f88920f313
Juanfran Correas <jfcorreas@gmail.com>
2016-11-04 00:37:58 +0100

    First test

--- spec/AdminConfig-Spec.js
+++ spec/AdminConfig-Spec.js
@@ -1,6 +1,8 @@
 
 describe("S1. Configurar tienda/articulo", function() {
+  var admin = require('../app/adminConfig.js');
   it ("1. Añadir un tipo de artículo", function() {
-    expect(false).toBe(true);
+    expect(admin.tipoArticulo).toBeDefined();
+    expect(admin.tipoArticulo).toBe("televisor");
   });
 });
