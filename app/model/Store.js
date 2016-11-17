42ce5a1b5759b2da66bb43602caaef7c6405900b
Juanfran Correas <jfcorreas@gmail.com>
2016-11-18 00:03:46 +0100

    Travis configured

---
+++ app/model/Store.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Store {
+  constructor(name, articles){
+    this.name = name;
+    this.articles = articles;
+  }
+}
+
+module.exports = Store;
--- spec/AdminConfig-Spec.js
+++ spec/AdminConfig-Spec.js
@@ -1,7 +1,8 @@
 
 var admin = require('../app/adminConfig.js');
-var ItemType = require('../app/model/ItemType.js');
+
 // Story 1: Configurar tienda/artículo
+var ItemType = require('../app/model/ItemType.js');
 
 describe("1. Añadir un tipo de artículo", function() {
   it ("1.1. Un tipo de artículo contiene nombre y precio", function(){
@@ -19,5 +20,15 @@ describe("1. Añadir un tipo de artículo", function() {
     admin.addItemType("videojuego");
     expect(admin.getItemTypes().length).toEqual(numberOfTypes+1);
     expect(admin.getItemTypes()).toContain("videojuego");
+  });
+});
+
+var Store = require('../app/model/Store.js');
+
+describe("2. Añadir una tienda", function() {
+  it ("2.2. Una tienda tiene nombre y lista de artículos", function() {
+    let store = new Store("tienda", [1,2,3,4]);
+    expect(store.name).toEqual(jasmine.any(String));
+    expect(store.articles).toEqual(jasmine.any(Array));
   });
 });
